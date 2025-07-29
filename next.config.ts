f30b171b44c954948026c7807143eefe8241b4f3
saurav dhande <dhandesaurav52@gmail.com>
2025-07-29 14:15:25 +0000

    Error: Invalid src prop (https://www.pexels.com/photo/clothes-hanged-on-

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pexels.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
