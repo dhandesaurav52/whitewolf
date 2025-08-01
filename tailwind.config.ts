28f34157690a02bc4b83e3ffd4813937c7791501
saurav dhande <dhandesaurav52@gmail.com>
2025-08-01 23:28:44 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -105,7 +105,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         "fade-in": "fade-in 0.5s ease-in-out",
         'marquee': 'marquee 30s linear infinite',
-        'marquee-reverse': 'marquee-reverse 30s linear infinite',
+        'marquee-reverse': 'marquee-reverse 30s linear infinite'
       },
     },
   },
